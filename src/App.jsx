46d812037488be16b511f4cd9a38b78996694aea
Dev <93630615+MateussantosVSr3@users.noreply.github.com>
2024-10-30 00:02:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
